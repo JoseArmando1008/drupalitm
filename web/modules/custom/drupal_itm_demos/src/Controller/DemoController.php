2b55ab99e8acca89a1c15390fa1c033d8bbb6d09
fakeholim <itsmilosmail@gmail.com>
2018-05-24 16:39:12 -0600

    tema 2.0

--- web/modules/custom/drupal_itm_demos/src/Controller/DemoController.php
+++ web/modules/custom/drupal_itm_demos/src/Controller/DemoController.php
@@ -16,10 +16,42 @@ class DemoController extends ControllerBase {
    *   Return Hello string.
    */
   public function testTheme() {
-    return [
-      '#type' => 'markup',
-      '#markup' => $this->t('Implement method: testTheme')
-    ];
+      
+      $items = [
+          [
+              'title' => $this->t('title 1'),
+              'description' => $this->t('description 1'),
+          ],
+          [
+              'title' => $this->t('title 2'),
+              'description' => $this->t('description 2'),
+          ],
+          [
+              'title' => $this->t('title 3'),
+              'description' => $this->t('description 3'),
+          ],
+      ];
+      $build = [
+          'items' => [],
+          '#prefix' => '<div id="accordion">',
+          '#sufix' => '</div>',
+          '#attached' => [
+              'library' => [
+                  'core/jquery.ui.accordion',
+                  'drupal_itm_demos/demo',
+              ],
+          ],
+      ];
+      
+      foreach ($items as $item) {
+          $build['items'][] = [
+              '#theme' => 'item_accordion',
+              '#title' => $item['title'],
+              '#description' => $item['description'],
+          ];
+      }
+      
+    return $build;
   }
 
 }
